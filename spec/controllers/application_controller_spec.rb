d75b49b58fb86a284d1fb1132f22a4943fa4b978
Thomas Marciniak <tomasz.marciniak@setapp.pl>
2018-07-04 13:36:15 +0200

    Tweak ApplicationController tests

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -6,6 +6,11 @@ RSpec.describe ApplicationController, type: :controller do
   before { request.env['HTTP_AUTHORIZATION'] = 'Bearer ' << jwt }
 
   describe '#authenticate' do
-    it { expect(subject.logged_in?).to be_truthy }
+    context 'when gets Bearer JWT token and logs user in' do
+      it { expect(subject.logged_in?).to be_truthy }
+    end
+    context 'when logged in and does not respond_with_error' do
+      it { expect(subject.authenticate).to be_nil }
+    end
   end
 end
