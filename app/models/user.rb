c63d0ca8d559c7d335baadbaa19d7898c6234879
Thomas Marciniak <tomasz.marciniak@setapp.pl>
2018-07-10 09:35:02 +0200

    Add following user functionality to model and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,24 @@ class User < ApplicationRecord
   include ActiveModel::Validations
 
   has_many :posts
+  has_many :followers, class_name: 'Follow', foreign_key: 'followed_id'
+  has_many :following, class_name: 'Follow', foreign_key: 'follower_id'
 
   has_secure_password
 
   validates :username, presence: true, length: { minimum: 4, maximum: 32 }, uniqueness: true
   validates :email, presence: true, email: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 7, maximum: 64 }
+
+  def follow(user_to_follow)
+    following.create(followed_id: user_to_follow.id)
+  end
+
+  def unfollow(user_to_unfollow)
+    following.find_by(followed_id: user_to_unfollow.id).destroy
+  end
+
+  def following?(user_to_check)
+    following.find_by(followed_id: user_to_check.id).present?
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  before(:all) { @user1 = create :user }
+  before(:all) { @valid_user = create :user }
   it { is_expected.to have_secure_password }
   it { is_expected.to have_many :posts }
+  it { is_expected.to have_many :followers }
+  it { is_expected.to have_many :following }
   it { is_expected.to validate_presence_of :email }
   it { is_expected.to validate_presence_of :username }
   it { is_expected.to validate_uniqueness_of :username }
   it { is_expected.to validate_uniqueness_of :email }
 
-  before { @user2 = build(:user, password: 'asd') }
   context 'when password validation failed' do
-    it { expect(@user2).not_to be_valid }
+    let!(:invalid_user) { build(:user, password: 'asd') }
+    it { expect(invalid_user).not_to be_valid }
+  end
+
+  describe 'following functionality' do
+    let!(:second_valid_user) { create(:user, username: 'icecube3', email: 'icecube3@gmail.com') }
+    context 'when there are no followers' do
+      it { expect(@valid_user.following.count). to eq(0) }
+    end
+    context 'when following another user' do
+      before { @valid_user.follow second_valid_user }
+      it { expect(@valid_user.following?(second_valid_user)).to be_truthy }
+    end
+    context 'when unfollowing another user' do
+      before { @valid_user.follow second_valid_user }
+      before { @valid_user.unfollow second_valid_user }
+      it { expect(@valid_user.following?(second_valid_user)).to be_falsey }
+    end
   end
 end
