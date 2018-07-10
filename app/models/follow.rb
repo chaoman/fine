045d1f1adf1683cbc09c9ea07025e0944bf0f14e
Thomas Marciniak <tomasz.marciniak@setapp.pl>
2018-07-10 09:38:42 +0200

    Fix lint issues

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,3 +1,4 @@
+# Model defining follow relations for +User+ model
 class Follow < ApplicationRecord
   belongs_to :follower, class_name: 'User', foreign_key: 'follower_id'
   belongs_to :following, class_name: 'User', foreign_key: 'followed_id'
