683de7aae3b7843763067cbe160cb93e36e9d2ae
Thomas Marciniak <tomasz.marciniak@setapp.pl>
2018-07-17 10:12:57 +0200

    Update docs

--- spec/acceptance/users_spec.rb
+++ spec/acceptance/users_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 require 'rspec_api_documentation/dsl'
 
 resource 'Users' do
+  header 'Authorization', :auth_token
+
   post '/users' do
     with_options scope: :user, with_example: true do
       parameter :username, required: true
@@ -22,6 +24,16 @@ resource 'Users' do
 
         do_request(request)
 
+        expect(status).to eq(200)
+      end
+    end
+  end
+
+  get '/users/me' do
+    let(:auth_token) { jwt_bearer }
+    context '200' do
+      example 'Fetching current user data' do
+        do_request
         expect(status).to eq(200)
       end
     end
